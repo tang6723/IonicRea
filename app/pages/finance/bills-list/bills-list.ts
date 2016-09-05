845336a7c352200f306b0d40126d34cff0d719ab
tang6723 <tang6723@hotmail.com>
2016-09-05 09:52:42 +0800

    tang

---
+++ app/pages/finance/bills-list/bills-list.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the BillsListPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/bills-list/bills-list.html',
+})
+export class BillsListPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
---
+++ app/pages/setting/goods-list/goods-list.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the GoodsListPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/goods-list/goods-list.html',
+})
+export class GoodsListPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
