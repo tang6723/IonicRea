925f72fb0ff4eb7f65bca9cdd8a35f27e040e60c
tang6723 <tang6723@hotmail.com>
2016-09-06 07:50:04 +0800

    tang

---
+++ app/pages/purchase/order-item/order-item.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the OrderItemPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/order-item/order-item.html',
+})
+export class OrderItemPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
---
+++ app/pages/purchase/order-list/order-list.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the OrderListPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/order-list/order-list.html',
+})
+export class OrderListPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
