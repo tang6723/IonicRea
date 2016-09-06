2e3dffe99ae7b3467d1b2917c4609159c87d674c
tang6723 <tang6723@hotmail.com>
2016-09-06 23:18:46 +0800

    Tang

--- app/pages/sales/contract-list/contract-list.ts
+++ app/pages/sales/contract-list/contract-list.ts
@@ -25,11 +25,11 @@ export class ContractListPage {
   ];
 
   private items2: any = [
-    {imgurl: 'ios-flag-outline', id: '123456', customer: '客户1',billno: '#201608040008', dateissue: '09-04', billstate: '草案',total: 30080 },
-    {imgurl: 'ios-flame-outline',id: '123457', customer: '客户2',billno: '#201608040007', dateissue: '09-04', billstate: '执行',total: 30080 },
-    {imgurl: 'ios-flash-outline',id: '123458', customer: '客户3',billno: '#201608040006', dateissue: '09-04', billstate: '审核',total: 30080 },
-    {imgurl: 'ios-flash-outline',id: '123459', customer: '客户4',billno: '#201608040005', dateissue: '09-04', billstate: '审核',total: 30080 },
-    {imgurl: 'ios-flash-outline',id: '123460', customer: '客户5',billno: '#201608040004', dateissue: '09-04', billstate: '结账',total: 30080 }
+    {imgurl: 'ios-flag-outline', id: '123456', customer: '客户1',billno: '#201608040008', dateissue: '08-04', billstate: '草案',total: 30080 },
+    {imgurl: 'ios-flame-outline',id: '123457', customer: '客户2',billno: '#201608040007', dateissue: '08-04', billstate: '执行',total: 30080 },
+    {imgurl: 'ios-flash-outline',id: '123458', customer: '客户3',billno: '#201608040006', dateissue: '08-04', billstate: '审核',total: 30080 },
+    {imgurl: 'ios-flash-outline',id: '123459', customer: '客户4',billno: '#201608040005', dateissue: '08-04', billstate: '审核',total: 30080 },
+    {imgurl: 'ios-flash-outline',id: '123460', customer: '客户5',billno: '#201608040004', dateissue: '08-04', billstate: '结账',total: 30080 }
   ];
 
   constructor(private navCtrl: NavController) {
