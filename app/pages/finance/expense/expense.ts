cd281f67317d0da3cc7de18869d58a42a8f57f8a
tang6723 <tang6723@hotmail.com>
2016-09-10 02:08:57 +0800

    Tang

--- app/pages/finance/expense/expense.ts
+++ app/pages/finance/expense/expense.ts
@@ -12,6 +12,14 @@ import { NavController } from 'ionic-angular';
 })
 export class ExpensePage {
 
+  billid:string="#201609040008";
+  billstate:string="草案";
+  billtype:string="销售";
+  billtotal:string ="3，380.00";
+  dateissue:string ="2016年9月3日";
+  customer:string ="微小宝";
+  customeraddress:string ="鞍山铁东湖南34号";
+
   constructor(private navCtrl: NavController) {
 
   }
