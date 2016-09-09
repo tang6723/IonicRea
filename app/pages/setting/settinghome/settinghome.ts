a55e6ba48680956ec34575c5d512949d40860512
tang6723 <tang6723@hotmail.com>
2016-09-09 12:43:25 +0800

    tang

--- app/pages/setting/settinghome/settinghome.ts
+++ app/pages/setting/settinghome/settinghome.ts
@@ -1,6 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import {EmployeePage} from '../employee/employee';
+import {EventsPage} from '../events/events';
+import {HumanPage} from '../human/human';
+import {ReportAssetPage} from '../report-asset/report-asset';
+import {ReportBalancePage} from '../report-balance/report-balance';
+import {ReportProfitPage} from '../report-profit/report-profit';
+import {ReportIncomePage} from '../report-income/report-income';
+import {ReportPayoutPage} from '../report-payout/report-payout';
+
+
 /*
   Generated class for the SettinghomePage page.
 
@@ -11,6 +21,11 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'build/pages/setting/settinghome/settinghome.html',
 })
 export class SettinghomePage {
+
+  employeePage=EmployeePage;
+  eventsPage=EventsPage;
+  humanPage=HumanPage;
+
 
   constructor(private navCtrl: NavController) {
 
