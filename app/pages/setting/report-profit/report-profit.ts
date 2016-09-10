f5826052946c41a9f02ab80d596b562c75163067
tang6723 <tang6723@hotmail.com>
2016-09-10 22:46:46 +0800

    Tang

--- app/pages/setting/report-profit/report-profit.ts
+++ app/pages/setting/report-profit/report-profit.ts
@@ -11,11 +11,90 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'build/pages/setting/report-profit/report-profit.html',
 })
 export class ReportProfitPage {
+  pet: string = "1";
+  stock:string = "stock1";
+  isAndroid: boolean = false;
+
+  appType = "Paid";
+  safari = "Shared Links";
+  weather = "sunny";
+
+  apps = {
+    "Paid": [
+      {
+        name: 'Monopoly',
+        price: '$0.99'
+      },
+      {
+        name: 'Angry Birds',
+        price: '$2.99'
+      }
+    ],
+    "Free": [
+      {
+        name: 'Snapchat',
+        price: 'GET'
+      },
+      {
+        name: 'Instagram',
+        price: 'OPEN'
+      }
+    ],
+    "Top": [
+      {
+        name: 'Spotify',
+        price: 'OPEN'
+      },
+      {
+        name: 'Pandora',
+        price: 'GET'
+      }
+    ]
+  };
+
+  items = {
+    "Bookmarks": [
+      {
+        name: 'Favorites',
+        icon: 'ios-star-outline'
+      },
+      {
+        name: 'History',
+        icon: 'ios-clock-outline'
+      }
+    ],
+    "Reading List": [
+      {
+        name: 'Terms of Service',
+        icon: 'create'
+      },
+      {
+        name: 'User Guide',
+        icon: 'book'
+      }
+    ],
+    "Shared Links": [
+      {
+        name: 'Ionic Framework',
+        icon: 'ionic'
+      },
+      {
+        name: 'Learn Angular',
+        icon: 'logo-angular'
+      }
+    ]
+  };
 
   constructor(private navCtrl: NavController) {
 
   }
+  getItems(type) {
+    return this.apps[type];
+  }
 
+  getSafariItems(type) {
+    return this.items[type];
+  }
   popHome(){
     this.navCtrl.popToRoot();
   }
