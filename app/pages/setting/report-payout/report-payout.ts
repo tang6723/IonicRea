cd6ad69b69eb80efeccda231cbc46505ec40efe1
dabai1980 <zhangyiwei1980@aliyun.com>
2016-09-10 23:25:32 +0800

    zhang

--- app/pages/setting/report-payout/report-payout.ts
+++ app/pages/setting/report-payout/report-payout.ts
@@ -1,24 +1,25 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, Platform} from 'ionic-angular';
 
 /*
-  Generated class for the ReportPayoutPage page.
+ Generated class for the ReportPayoutPage page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+ See http://ionicframework.com/docs/v2/components/#navigation for more info on
+ Ionic pages and navigation.
+ */
 @Component({
   templateUrl: 'build/pages/setting/report-payout/report-payout.html',
 })
 export class ReportPayoutPage {
 
-   typeList: string = 'class';
+  typeList: string = "class";
+  isAndroid: boolean = false;
 
-  constructor(private navCtrl: NavController) {
-
+  constructor(private navCtrl: NavController, private platform: Platform) {
+    this.isAndroid = platform.is('android');
   }
 
-  popHome(){
+  popHome() {
     this.navCtrl.popToRoot();
   }
 
