a00fff8da752037bcc73dfa919e662cbbbc08d5e
dabai1980 <zhangyiwei1980@aliyun.com>
2016-09-11 21:59:31 +0800

    zhang

--- app/pages/finance/expense-list/expense-list.ts
+++ app/pages/finance/expense-list/expense-list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {ExpensePage} from '../expense/expense';
 
 /*
   Generated class for the ExpenseListPage page.
@@ -12,8 +13,18 @@ import { NavController } from 'ionic-angular';
 })
 export class ExpenseListPage {
 
+  expenseList: any = [
+    {name:"张三", remark: "XXX项目差旅费", post:"销售", total:"1234", date:"2016-07-02"},
+    {name:"李四", remark: "礼品", post:"经理", total:"12343", date:"2016-05-02"},
+    {name:"王五", remark: "XXX项目差旅费", post:"销售", total:"1234", date:"2016-03-02"}
+  ];
+
   constructor(private navCtrl: NavController) {
 
+  }
+
+  itemSelected(item: any){
+    this.navCtrl.push(ExpensePage, '');
   }
 
 }
--- app/pages/finance/expense/expense.ts
+++ app/pages/finance/expense/expense.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides, AlertController, NavParams } from 'ionic-angular';
+
+import {ExpenseItemPage} from '../expense-item/expense-item';
+import {ExpenseListPage} from '../expense-list/expense-list';
 
 /*
   Generated class for the ExpensePage page.
@@ -12,6 +15,8 @@ import { NavController } from 'ionic-angular';
 })
 export class ExpensePage {
 
+  @ViewChild("myClider") slider: Slides;
+
   billid:string="#201609040008";
   billstate:string="草案";
   billtype:string="销售";
@@ -20,8 +25,82 @@ export class ExpensePage {
   customer:string ="微小宝";
   customeraddress:string ="鞍山铁东湖南34号";
 
-  constructor(private navCtrl: NavController) {
+  typeTmp: string;
+  employeeList: any;
 
+  initializeEmployeeList(){
+    this.employeeList = [
+      {id:12345, name:"张三", post: "销售"},
+      {id:12346, name:"李四", post: "销售"},
+      {id:12347, name:"王五", post: "经理"},
+      {id:12348, name:"赵六", post: "会计"}
+    ];
+  };
+
+  constructor(private navCtrl: NavController, private alertCtrl: AlertController, private  navParams: NavParams) {
+    this.initializeEmployeeList();
+  }
+
+  pushItem(index: number)
+  {
+
+    switch (index) {
+      case 1:
+        this.typeTmp = '申请';
+        break;
+      case 2:
+        this.typeTmp = '审批';
+        break;
+      case 3:
+        this.typeTmp = '付款';
+        break;
+    }
+
+    this.navCtrl.push(ExpenseItemPage,{
+      id:this.billid,
+      type:this.typeTmp
+
+    });
+  }
+
+  showEmployeeRadio() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('客户');
+    for(var i=0;i<this.employeeList.length;i++){
+      alert.addInput({
+        type:'radio',
+        label: this.employeeList[i].name ,
+        value:  this.employeeList[i].id
+      });
+    }
+    alert.addButton('取消');
+    alert.addButton({
+      text: '确定',
+      handler: data => {
+        for(var i=0;i<this.employeeList.length;i++) {
+          if(this.employeeList[i].id == data)
+          {
+            this.customer = this.employeeList[i].name;
+            this.customeraddress = this.employeeList[i].post;
+          }
+        }
+
+        console.log(data);
+      }
+    });
+    alert.present();
+  }
+
+  pushMore(){
+    this.navCtrl.push(ExpenseListPage, {});
+  };
+
+  gotoSlider(i: number){
+    this.slider.slideTo(i, 300);
+  }
+
+  popHome(){
+    this.navCtrl.popToRoot();
   }
 
 }
