bf23006a282cb4ed289954c0cc64337ed2a9c210
dabai1980 <zhangyiwei1980@aliyun.com>
2016-10-16 20:32:48 +0800

    zhang

---
+++ app/pages/raise-item/raise-item.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the RaiseItemPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/raise-item/raise-item.html',
+})
+export class RaiseItemPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
